209c97a6eb2284c6762ff3f500eb0b64686fef32
Brian <bmaitner@gmail.com>
2022-08-25 23:12:47 -0400

    added filter for < 3 hours to road

--- sampling_design_v2.R
+++ sampling_design_v2.R
@@ -77,118 +77,169 @@ source("https://raw.githubusercontent.com/AdamWilsonLab/emma_envdata/main/R/robu
     
 ##############################################################
   
-  #Filtering for slope
-
-  #get DEM
-    robust_pb_download(file = "nasadem.tif",
-                       dest = "temp/nasadem",
-                       repo = "AdamWilsonLab/emma_envdata",
-                       tag = "raw_static",
-                       overwrite = TRUE,
-                       max_attempts = 10,
-                       sleep_time = 30)
+# Filtering for slope
   
-  #load dem
-    dem <- terra::rast("temp/nasadem/nasadem.tif")
-    plot(dem)
+    # 
+    #   #get DEM
+    #     robust_pb_download(file = "nasadem.tif",
+    #                        dest = "temp/nasadem",
+    #                        repo = "AdamWilsonLab/emma_envdata",
+    #                        tag = "raw_static",
+    #                        overwrite = TRUE,
+    #                        max_attempts = 10,
+    #                        sleep_time = 30)
+    #   
+    #   #load dem
+    #     dem <- terra::rast("temp/nasadem/nasadem.tif")
+    #     plot(dem)
+    #     
+    #   # cut down to only the parks(and maybe a small buffer for edge effects) to save memory
+    #     dem <-
+    #     dem %>%
+    #       terra::mask(mask = sampling_locations %>%
+    #              st_buffer(dist = 1000) %>%
+    #              st_transform(crs = st_crs(dem)) %>%
+    #              vect())
+    #              
+    #     # terra::tmpFiles(current = FALSE,
+    #     #                 orphan = TRUE,
+    #     #                 old = TRUE,
+    #     #                 remove = TRUE)
+    #     
+    #     
+    #   # Need DEM in units of meters, so have to transform   
+    #     dem <- terra::project(dem, y = crs(domain))
+    # 
+    #     #need to break up dem to avoid "cannot allocate vector of X Gb" warning
+    #       
+    #     poly_for_dem <-
+    #       sampling_locations %>%
+    #         st_buffer(dist = 1000) %>%
+    #         st_transform(crs = st_crs(dem)) %>%
+    #         st_union() %>%
+    #       st_make_valid() %>%
+    #       st_cast("POLYGON") %>%
+    #       as.data.frame()
+    #     
+    #   for(i in 1:nrow(poly_for_dem)){
+    #     
+    #     poly_i <- poly_for_dem[i,]
+    # 
+    #     dem_i <- dem %>%
+    #       mask(mask = vect(poly_i))
+    #     
+    #     dem_i <- 
+    #       dem_i %>%
+    #       crop(poly_i)
+    #     
+    #     #plot(dem_i)
+    #     
+    #     #calc slope using insol package
+    #       
+    #       slope_insol <- insol::slope(cgrad(raster::raster(dem_i)), degrees = TRUE)
+    #       slope_insol <- raster::raster(slope_insol,
+    #                                    crs=raster::projection(dem_i))
+    #       raster::extent(slope_insol) <- raster::extent(raster::raster(dem_i))
+    #       plot(slope_insol)
+    #   
+    # 
+    #     #calc slop using stars package
+    #       
+    #       slope_stars <- starsExtra::slope(x = stars::st_as_stars(dem_i,proxy=FALSE))    
+    #       slope_stars <- rast(slope_stars)
+    #       #plot(slope_stars)
+    # 
+    #     if(i == 1){
+    #       
+    #       stars_slope <- slope_stars
+    #       insol_slope <- slope_insol
+    #       
+    #     }else{
+    #       
+    #       stars_slope <- terra::merge(stars_slope, slope_stars)
+    #       insol_slope <- terra::merge(insol_slope, slope_insol)
+    #       
+    #     }
+    #     
+    #     rm(slope_stars, slope_insol, dem_i, poly_i)
+    #     
+    #     #clear out temp files to keep hd from filling up
+    #     terra::tmpFiles(current = FALSE,
+    #                     orphan = TRUE,
+    #                     old = TRUE,
+    #                     remove = TRUE)
+    # 
+    #   }
+    # 
+    # gc()
+    # 
+    # # writeRaster(x = insol_slope, filename = "data/output/insol_slope.tif")
+    # # writeRaster(x = stars_slope, filename = "data/output/stars_slope.tif")
+    #     
+    # insol_slope <- rast("data/output/insol_slope.tif")
+    # stars_slope <- rast("data/output/stars_slope.tif")
+    # 
+    # #make a polygon of sites that have a reasonable slope in each raster (< 30 degrees)
+    # 
+    #   insol_slope <- (insol_slope < 30)
+    #   insol_slope[which(is.na(values(insol_slope)))] <- 0
+    #   
+    #   stars_slope <- (stars_slope < 30)
+    #   stars_slope[which(is.na(values(stars_slope)))] <- 0
+    #   
+    #   #insol doesn't keep the crs, so add it in here
+    #     crs(insol_slope) <- crs(stars_slope)
+    # 
+    #   combined_slope <- insol_slope + stars_slope
+    #   
+    #   #plot(combined_slope) #pretty good correspondence. we'll keep any that meet the criteria using either approach
+    #   combined_slope <- (combined_slope > 0)
+    #   combined_slope[which(values(combined_slope)==0)] <- NA
+    #   
+    #   
+    #   plot(combined_slope)
+    #   
+    #   slope_poly <-
+    #     combined_slope %>%
+    #       terra::as.polygons(trunc = TRUE,
+    #                   dissolve = TRUE,
+    #                   values = TRUE) %>%
+    #     st_as_sf()
+    # # 
+    # #         st_write(obj = slope_poly,
+    # #                 dsn = "data/output/flat_polygons.gpkg")
     
-  # cut down to only the parks(and maybe a small buffer for edge effects) to save memory
-    dem <-
-    dem %>%
-      terra::mask(mask = sampling_locations %>%
-             st_buffer(dist = 1000) %>%
-             st_transform(crs = st_crs(dem)) %>%
-             vect())
-             
-    # terra::tmpFiles(current = FALSE,
-    #                 orphan = TRUE,
-    #                 old = TRUE,
-    #                 remove = TRUE)
+    slope_poly <- st_read("data/output/flat_polygons.gpkg")
     
+    #add a slight buffer at a bit larger than pixel size so that we cut out areas where its just a pixel or two of steep terrain.
+    slope__poly_buffered <-
+      slope_poly %>%
+      st_buffer(dist = 40)
     
-  # Need DEM in units of meters, so have to transform   
-    dem <- terra::project(dem, y = crs(domain))
-
-    #need to break up dem to avoid "cannot allocate vector of X Gb" warning
-      
-    poly_for_dem <-
-      sampling_locations %>%
-        st_buffer(dist = 1000) %>%
-        st_transform(crs = st_crs(dem)) %>%
-        st_union() %>%
-      st_make_valid() %>%
-      st_cast("POLYGON") %>%
-      as.data.frame()
-    
-  for(i in 1:nrow(poly_for_dem)){
-    
-    poly_i <- poly_for_dem[i,]
-
-    dem_i <- dem %>%
-      mask(mask = vect(poly_i))
-    
-    dem_i <- 
-      dem_i %>%
-      crop(poly_i)
-    
-    #plot(dem_i)
-    
-    #calc slope using insol package
-      
-      slope_insol <- insol::slope(cgrad(raster::raster(dem_i)), degrees = TRUE)
-      slope_insol <- raster::raster(slope_insol,
-                                   crs=raster::projection(dem_i))
-      raster::extent(slope_insol) <- raster::extent(raster::raster(dem_i))
-      plot(slope_insol)
-  
-
-    #calc slop using stars package
-      
-      slope_stars <- starsExtra::slope(x = stars::st_as_stars(dem_i,proxy=FALSE))    
-      slope_stars <- rast(slope_stars)
-      #plot(slope_stars)
-
-    if(i == 1){
-      
-      stars_slope <- slope_stars
-      insol_slope <- slope_insol
-      
-    }else{
-      
-      stars_slope <- terra::merge(stars_slope, slope_stars)
-      insol_slope <- terra::merge(insol_slope, slope_insol)
-      
-    }
-    
-    rm(slope_stars, slope_insol, dem_i, poly_i)
-    
-    #clear out temp files to keep hd from filling up
-    terra::tmpFiles(current = FALSE,
-                    orphan = TRUE,
-                    old = TRUE,
-                    remove = TRUE)
-
-  }
-
-gc()
-
-# writeRaster(x = insol_slope, filename = "data/output/insol_slope.tif")
-# writeRaster(x = stars_slope, filename = "data/output/stars_slope.tif")
-    
-insol_slope <- rast("data/output/insol_slope.tif")
-stars_slope <- rast("data/output/stars_slope.tif")
-
-#make a polygon of sites that have a reasonable slope in each raster (< 30 degrees)
-
-plot(insol_slope)
-insol_slope %>%
-  mask(mask = insol_slope<30) %>%
-  as.polygons()->insol_poly
-  plot()
-
-
 #################
 
+    #Let's make omit sites > 3 hours from the road
+    
+    time_to_sites <- rast("data/output/distance_h_to_sites.tif")
+    time_to_sites <- (time_to_sites < 3)
+    time_to_sites[time_to_sites == 0] <- NA
+    
+    time_to_sites %>%
+    terra::as.polygons() %>%
+      st_as_sf()-> time_to_sites
+    
+crs(time_to_sites)@projargs==crs(domain)@projargs
+
+
+#################    
+
+  rm(recent_ndvi, road_site_ndvis, slope_poly)  
+        terra::tmpFiles(current = FALSE,
+                        orphan = TRUE,
+                        old = TRUE,
+                        remove = TRUE)
+  gc()
+    
   plot(sampling_near_roads)  
   
       
@@ -198,19 +249,17 @@ insol_slope %>%
       #only include sites with an ndvi > 0.2
     st_intersection(y = st_make_valid(focal_sites_good_ndvi)) %>%
       
-      #only include sites that have a slope less than
-    st_intersection(st_intersection()  
-    st_union()%>%
+      #only include sites that have a slope < 30 degrees
+    st_intersection(y = slope__poly_buffered) %>%  
       
+      # only include sites within 3 hours of the road
+    st_intersection(y= time_to_sites %>% st_make_valid()) %>%  
       
-      plot()
-    
-    
+      #union and create a new object
+    st_union() -> acceptable_sites
 
-    #next, exclude sites with slopes above cutoff in both slope rasters
+    plot(acceptable_sites)
     
-  plot(sampling_near_roads)  
-  
 
     
     
