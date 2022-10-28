0918e0b23a97fa84e379b48337ffbb936c8a6992
bmaitner <bmaitner@gmail.com>
2022-10-28 13:51:05 -0400

    fighting with rgee

--- R/mock_flights_earth_engine.R
+++ R/mock_flights_earth_engine.R
@@ -6,12 +6,13 @@
 #install rgee python dependencies
   rgee::ee_install()
   rgee::ee_install_upgrade()
-  reticulate::py_install('earthengine-api==0.1.323', envname='rgee',ignore_installed = TRUE)
-  reticulate::py_install('earthengine-api==0.1.323')
-  reticulate::py_
   rgee::ee_check()
-  
-  ?py_install
+
+
   #Initialize rgee
   ee_Initialize(drive = TRUE)
-  
+
+
+  #curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-387.0.0-linux-x86_64.tar.gz
+  #tar -xf google-cloud-cli-387.0.0-linux-x86_64.tar.gz
+  #./google-cloud-sdk/install.sh
