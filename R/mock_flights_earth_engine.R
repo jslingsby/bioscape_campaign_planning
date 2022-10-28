c82d875fd8156051b6afa3aee5c71caf804fc64b
Brian <bmaitner@gmail.com>
2022-10-28 13:53:26 -0400

    fighitng with ee

--- R/mock_flights_earth_engine.R
+++ R/mock_flights_earth_engine.R
@@ -4,14 +4,12 @@
 
 
 #install rgee python dependencies
+  rgee::ee_clean_pyenv()
   rgee::ee_install()
   rgee::ee_install_upgrade()
-  reticulate::py_install('earthengine-api==0.1.323', envname='rgee',ignore_installed = TRUE)
-  reticulate::py_install('earthengine-api==0.1.323')
-  reticulate::py_
   rgee::ee_check()
   
   ?py_install
   #Initialize rgee
-  ee_Initialize(drive = TRUE)
-  
+  #ee_Initialize(drive = TRUE)
+  ee_Initialize()
